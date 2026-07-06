a82c8ae12c06726f8331102ee52c653676fc28e9
Ntharusha <ntb069@gmail.com>
2026-07-06 13:04:07 +0530

    chore: initial project architecture and templates

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3001',
+      '/socket.io': {
+        target: 'http://localhost:3001',
+        ws: true
+      }
+    }
+  }
+});
